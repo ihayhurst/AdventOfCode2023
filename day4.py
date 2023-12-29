d0d72e5ea20211b7f986323e44b2a3cfdc3a906a
Ian Hayhurst <ihayhurst@users.noreply.github.com>
2023-12-29 18:20:38 +0000

    testing small changes for ricing

--- day4.py
+++ day4.py
@@ -7,15 +7,19 @@ import time
 
 start = time.time()
 sum = 0
+lines=[]
 with open("day4-input.txt", "rt") as f:
-    input = f.readlines()
-    for line in input:
-        data = line.split("|")
-        winning_nums = [int(d) for d in data[0].split() if d.isdigit()]
-        our_nums = [int(d) for d in data[1].split() if d.isdigit()]
-        result = [w for w in our_nums if w in winning_nums]
-        if result:
-            sum += 2 ** (len(result) - 1)
+    for file_line in f:
+        lines.append(file_line.strip())
+
+# file_line = f.readlines()
+for line in lines:
+    data = line.split("|")
+    winning_nums = [int(d) for d in data[0].split() if d.isdigit()]
+    our_nums = [int(d) for d in data[1].split() if d.isdigit()]
+    result = [w for w in our_nums if w in winning_nums]
+    if result:
+        sum += 2 ** (len(result) - 1)
 
 print(sum)
 delta = time.time() - start
--- day4p2.py
+++ day4p2.py
@@ -1,6 +1,6 @@
 """
 Advent of Code 2023 day 4
-Task 2 8477787 Time Taken 5.94ms
+Task 2 8477787 Time Taken 5.75ms
 """
 import time
 import numpy as np
@@ -28,12 +28,13 @@ def parse_input(FILE):
     cards=[]
     with open(FILE, "rt") as f:
         input = f.readlines()
-        for line in input:
-            data = line.split("|")
-            winning_nums = [int(d) for d in data[0].split() if d.isdigit()]
-            our_nums = [int(d) for d in data[1].split() if d.isdigit()]
-            result =[w for w in our_nums if w in winning_nums]
-            cards.append(len(result))
+
+    for line in input:
+        data = line.split("|")
+        winning_nums = [int(d) for d in data[0].split() if d.isdigit()]
+        our_nums = [int(d) for d in data[1].split() if d.isdigit()]
+        result =[w for w in our_nums if w in winning_nums]
+        cards.append(len(result))
     return cards
 
 if __name__ == "__main__":
